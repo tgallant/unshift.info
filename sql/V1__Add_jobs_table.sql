b3912328b6ef6d52a2471ac834ef3d0358173d6d
Tim Gallant <me@timgallant.us>
2019-11-16 20:47:12 -0800

    Updates initial jobs table schema

--- sql/V1__Add_jobs_table.sql
+++ sql/V1__Add_jobs_table.sql
@@ -1,6 +1,7 @@
 create table jobs (
   id serial not null primary key,
   args jsonb,
-  processing boolean,
-  complete boolean
+  status text,
+  created_at timestamp with time zone default current_timestamp,
+  updated_at timestamp with time zone default current_timestamp
 );
